107f9791ddd29feb8d160b1080fc685463c371f1
Valentin Kosyakov <valentin.kosyakovEnd@yandex.ru>
2023-05-20 20:39:03 +0300

    add weather api in header

--- src/Components/Header/Weather/Weather.jsx
+++ src/Components/Header/Weather/Weather.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import s from './weather.module.scss'
+import {useWeather} from "../../../store/Weather/store.js";
 
 const Weather = () => {
+    const { getWeather, temperature } = useWeather();
+    const getTemperature = async () =>{
+        await getWeather()
+    }
+    useEffect(() => {
+        getTemperature()
+    }, [])
     return (
         <div className={s.container}>
             <div className={s.image}>Облако</div>
             <div className={s.weather}>
                 <p>Сейчас</p>
-                <p>+13</p>
+                <p>{temperature[0]}</p>
             </div>
         </div>
     );
